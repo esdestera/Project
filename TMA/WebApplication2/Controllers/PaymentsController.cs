3df21d2b1651a13a2d1a7a0ee700292e31d7dff5
Scurtu Estera Daniela <estera9704@gmail.com>
2018-04-01 10:48:46 +0300

    am facut cateva schimbari la pagina

---
+++ TMA/WebApplication2/Controllers/PaymentsController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class PaymentsController : Controller
+    {
+        public ActionResult PaymentsToMe()
+        {
+            return View();
+        }
+
+        public ActionResult PaymentsToOthers()
+        {
+            return View();
+        }
+
+        public ActionResult MyPayments()
+        {
+            return View();
+        }
+    }
+}
