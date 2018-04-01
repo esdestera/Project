9bf4a66c35e54620cab0ff46ba95ca209fe651ae
Scurtu Estera Daniela <estera9704@gmail.com>
2018-04-01 11:10:15 +0300

    adaugat services

---
+++ TMA/WebApplication2/Controllers/ServicesController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public ActionResult Bills()
+        {
+            return View();
+        }
+
+        public ActionResult Taxes()
+        {
+            return View();
+        }
+
+        public ActionResult PhoneCreditCharge()
+        {
+            return View();
+        }
+
+        public ActionResult MyServices()
+        {
+            return View();
+        }
+    }
+}
